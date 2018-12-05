4f693ed5ffd99e07ea6d96dd8a718746c1a83295
belle9410 <45545665+belle9410@users.noreply.github.com>
2018-12-06 08:32:12 +1000

    Add files via upload

--- web/app.js
+++ web/app.js
@@ -1,7 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	pg = require('pg'),
+	app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+//DB connection
+var connect = "postgres://belle:xxl941012@localhost/sample_db";
+var pool = new pg.Pool(connect);
+
+//set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/', function(req, res){
+	pool.connect(connect, function(err,client,done) { 
+		if(err) { 
+			return console.error('error fetching client from pool', err); 
+		} 
+		client.query('SELECT * FROM userinfo', function(err, result) { 
+			if(err) { 
+			return console.error('error running query', err); 
+		} 
+		res.render('login',{userinfo: result.rows});
+		done(); 
+		});
+	}); 
+	pool.end();
+});
+
+
+//server
+app.listen(3000, function(){
+	console.log('Server Started On Port 3000');
+});
