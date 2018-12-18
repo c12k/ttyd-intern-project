79b4dbab9a254907d39b707c3d01f4847b46be5a
Daniel <dcopp.yoomtah@gmail.com>
2018-12-18 14:16:05 +1000

    Added clicking back button on web register page to test

--- devops/Test-scripts/web-tests/test_web_page_selenium_pytest.py
+++ devops/Test-scripts/web-tests/test_web_page_selenium_pytest.py
@@ -4,13 +4,20 @@ from selenium.webdriver import FirefoxOptions
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 driver = webdriver.Firefox(firefox_options=opts)
-driver.get('http://172.28.5.1:3000')
+# Get the homepage
+driver.get("http://172.28.5.1:3000")
 print(driver.title)
 print(driver.current_url)
 assert "test project" in driver.title
+assert driver.find_element_by_class_name("login-content")
+# Click the signup button and check we're in the register page
 driver.find_element_by_xpath("//button[text()='Sign Up']").click()
 print(driver.title)
 print(driver.current_url)
 assert "register" in driver.current_url
+# Click the back button and make sure we're back on the home page
 assert driver.find_element_by_class_name("register-form")
+driver.find_element_by_id("register-back-btn").click()
+assert driver.find_element_by_class_name("login-content")
+
 driver.close()
