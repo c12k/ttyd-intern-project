d11fc4bd8f6235f6e072d5b605ffd97d8cfa1eae
Colin <colin.mccririck@gmail.com>
2018-12-19 16:11:45 +1000

    added rasa core test

--- devops/Test-scripts/nlu-tests/test_nlu_page_selenium_pytest.py
+++ devops/Test-scripts/nlu-tests/test_nlu_page_selenium_pytest.py
@@ -7,3 +7,6 @@ driver = webdriver.Firefox(firefox_options=opts)
 driver.get('http://172.28.5.3:5000')
 print(driver.current_url)
 assert driver.find_element_by_tag_name("body").text == "hello from Rasa NLU: 0.13.7"
+driver.get('http://172.28.5.4:5005')
+print(driver.current_url)
+assert driver.find_element_by_tag_name( "body").text == "hello from Rasa Core: 0.12.0"
